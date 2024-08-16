bdca5069eba8456df906c5ddd6cc38e36bdb4b66
Javier Contreras <javierconts3@live.com>
2024-08-16 19:56:29 -0400

    chore: first example

---
+++ README.md
@@ -0,0 +1,20 @@
+# Graphene Library Demo
+This repository contains a demo for the Graphene library, a powerful graphics library for creating interactive user interfaces. The demo showcases various features of the library, including drawing primitives, layout system, multi-page user interfaces, and custom widgets.
+
+## Examples
+
+### Drawing Primitives
+
+The demo includes examples of drawing primitives such as lines, rectangles, circles, and polygons. These primitives can be customized with different colors, sizes, and styles to create visually appealing graphics.
+
+### Layout System
+
+Graphene library provides a flexible layout system that allows you to easily arrange and position UI elements. The demo demonstrates various layout options, including Rows, Columns, and absolute positioning.
+
+### Multi-Page User Interfaces
+
+With Graphene, you can create multi-page user interfaces with ease. The demo showcases how to navigate between different pages and manage state transitions using the library's built-in navigation features.
+
+### Custom Widgets
+
+Graphene library allows you to create custom widgets tailored to your specific needs. The demo includes examples of custom widgets, demonstrating how to extend the library's functionality and create unique user experiences.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,74 @@
 #include <Graphene.h>
 #include <mbed.h>
 
+#include <chrono>
+
+void splashScreen(GrapheneDrivers::STM32F769I_Canvas &screenDriver,
+				  rtos::Kernel::Clock::duration_u32 timeout = 2000ms) {
+	screenDriver.clear(Graphene::NAVY);
+	screenDriver.drawString(
+		screenDriver.getBounds(), "STM32F769I-DISCO", Graphene::WHITE,
+		Graphene::NAVY, Graphene::SYSTEM::getSystemFont("ubuntu_light_35px"),
+		Graphene::TextAlignment::CENTER);
+
+	Graphene::Rect grapheneRect = screenDriver.getBounds();
+	grapheneRect.setY(50);
+	grapheneRect.setHeight(grapheneRect.getHeight() - 50);
+
+	screenDriver.drawString(
+		grapheneRect, "Graphene", Graphene::WHITE, Graphene::NAVY,
+		Graphene::SYSTEM::getSystemFont("ubuntu_light_23px"),
+		Graphene::TextAlignment::CENTER);
+
+	screenDriver.drawLine({0, 0},
+						  {screenDriver.getWidth(), screenDriver.getHeight()},
+						  Graphene::WHITE);
+
+	ThisThread::sleep_for(timeout);
+}
+
+void shapes(GrapheneDrivers::STM32F769I_Canvas &screenDriver,
+			rtos::Kernel::Clock::duration_u32 timeout = 5000ms) {
+	screenDriver.clear(Graphene::NAVY);
+
+	screenDriver.drawRectangle({10, 10}, 100, 100, Graphene::WHITE);
+	screenDriver.drawCircle({200, 200}, 50, Graphene::RED);
+	screenDriver.drawLine({0, 0},
+						  {screenDriver.getWidth(), screenDriver.getHeight()},
+						  Graphene::GREEN);
+	screenDriver.drawPolygon({{150, 100}, {200, 130}, {120, 150}},
+							 Graphene::PINK);
+
+	screenDriver.fillRectangle({400, 80}, 100, 100, Graphene::LIME_GREEN);
+	screenDriver.fillCircle({500, 300}, 50, Graphene::AQUA);
+	screenDriver.fillPolygon({{450, 200}, {500, 230}, {420, 250}},
+							 Graphene::YELLOW);
+
+	ThisThread::sleep_for(timeout);
+}
+
+void uiThread() {
+	GrapheneDrivers::STM32F769I_Canvas screenDriver;
+	GrapheneDrivers::STM32F769I_Touch touchScreenDriver;
+	splashScreen(screenDriver);
+	shapes(screenDriver);
+
+	// PagesView mainView(screenDriver.getBounds());
+	// Graphene::GrapheneApp app(&screenDriver, &touchScreenDriver, &mainView);
+
+	screenDriver.clear(Graphene::DARK_GRAY);
+
+	while(true) {
+		// app.eventLoop();
+		ThisThread::sleep_for((rtos::Kernel::Clock::duration_u32)33ms);
+	}
+}
+
 int main() {
-	while (1) {
-		printf("Hello World!\n");
+	Thread uiThreadHandle(osPriorityNormal, 4096, nullptr, "uiThread");
+	uiThreadHandle.start(uiThread);
+
+	while (true) {
+		ThisThread::yield();
 	}
 }
